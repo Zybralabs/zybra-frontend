43792518eea3bf47210a973d11fefb3deee031b1
Webners1 <faizan_babar70@yahoo.com>
2024-12-13 18:15:12 +0500

    add

--- src/components/MainPane/index.ts
+++ src/components/MainPane/index.ts
@@ -1,15 +1,1 @@
-import React from 'react';
-import Sidebar from './components/Sidebar';
-
-const App = () => {
-  return (
-    <div className="min-h-screen flex bg-dark text-white">
-      <Sidebar />
-      <div className="flex-1">
-        <Dashboard />
-      </div>
-    </div>
-  );
-};
-
-export default App;
+export { default as MainPane } from "./MainPane";
